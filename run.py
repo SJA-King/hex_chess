b675c4fa3d85ab03cbe757e288b83dd289aeb08e
Simon King <Simon.King@ncipher.com>
2023-09-16 17:45:27 +0100

    add queen class and the starting positions for queens for both players

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from src.classes.hexes import HexTile
 from src.classes.pieces.pawn import Pawn
 from src.classes.pieces.bishop import Bishop
 from src.classes.pieces.king import King
+from src.classes.pieces.queen import Queen
 from src.classes.constants import HexColours, PlayerColour
 from src.classes.position import Position
 
@@ -122,7 +123,14 @@ def main():
                                                              position=Position(1, 4, -5),
                                                              hex_height=hex_height,
                                                              hex_width=hex_width)
-
+    hexagons[Position(-1, -4, 5)].piece_on_hex = Queen(colour=PlayerColour.BLACK,
+                                                     position=Position(-1, -4, 5),
+                                                     hex_height=hex_height,
+                                                     hex_width=hex_width)
+    hexagons[Position(-1, 5, -4)].piece_on_hex = Queen(colour=PlayerColour.WHITE,
+                                                     position=Position(-1, 5, -4),
+                                                     hex_height=hex_height,
+                                                     hex_width=hex_width)
     terminated = False
     while not terminated:
         for event in pygame.event.get():
